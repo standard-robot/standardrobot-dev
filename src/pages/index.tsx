52723771b4c2ae9ff7fdd6de5265162b9b5ba7e4
astandardrobot <jonahjperry5@gmail.com>
2023-01-03 01:46:47 -0600

    updated tsconfig to include css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
