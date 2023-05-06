4dd07a0433dfe3cb77ead5834af4eefdf189f64c
standard-robot <jonahjperry5@gmail.com>
2023-05-06 04:33:27 -0500

    added proper link to subdomain

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import NextLink from 'next/link';
 import Image from 'next/image'
+import Link from 'next/link';
 import { inter } from '@/styles/fonts'
 
 export default function Home() {
@@ -90,22 +92,10 @@ export default function Home() {
                     </p>
                 </a>
 
-                <a
-                    href="https://blog.standardrobot.dev"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Blog{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        stuff I wrote
-                    </p>
-                </a>
+                <Link className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    href="https://blog.standardrobot.dev">
+                    Blog
+                </Link>
             </div>
         </main>
     )
