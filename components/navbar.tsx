d8ca88d826506a5a048a8c163030a03d672f3d84
standard-robot <jonahjperry5@gmail.com>
2023-07-04 03:28:20 -0500

    updated again

---
+++ components/navbar.tsx
@@ -0,0 +1,4 @@
+import React from "react"
+export default const Page() => {
+    return (<h1>test</h1>)
+}
---
+++ components/test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const test = () => {
+  return <h1>WAAAAH</h1>;
+};
