6338c3d00f456152adc60feae43ecd90f1aea24b
standard-robot <jonahjperry5@gmail.com>
2023-08-01 02:00:18 -0500

    added typed effects

---
+++ components/typed.tsx
@@ -0,0 +1,30 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
+export const Typewriter = ({ text, delay, infinite }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    let timeout;
+
+    if (currentIndex <= text.length) {
+      timeout = setTimeout(() => {
+        setCurrentText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, delay);
+    } else if (infinite) {
+      // ADD THIS CHECK
+      setCurrentIndex(0);
+      setCurrentText('');
+    }
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, infinite, text]);
+
+  return <span>{currentText}</span>;
+};
+
+export default function BlinkingCursor() {
+  return <span className="blinking-cursor">|</span>;
+}
