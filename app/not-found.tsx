5b5c044f689c987e07ed04eb9133fc7fb5a05926
standard-robot <jonahjperry5@gmail.com>
2023-08-08 11:49:39 -0500

    adjusted styling for bsod

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import FullScreenGlitch from '../components/visuals';
 export default function BlueScren() {
   return (
     <body className="bg-bsod">
-      <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center min-w-screen min-h-screen">
         <div className="fixed items-center justify-center top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-border w-2/12 h-1/8">
           <h1 className="font-w95fa text-center text-bsod text-4xl">uh - oh</h1>
         </div>
@@ -14,7 +14,7 @@ export default function BlueScren() {
             look at what you fucking did. You navigated to a nonexistent page.
           </p>
           <p className="mt-20 ">nothing but a bsod here.</p>
-          <p className="mt-20">Go back</p>
+          <p className="mt-10">Go back</p>
         </div>
       </div>
     </body>
